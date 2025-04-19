dcac246b42ea7afc848affdc7b6b89f24348046b
nbhhdd <3482053734@qq.com>
2025-04-19 15:37:03 +0800

    Update 演讲比赛流程管理系统.cpp

--- 基于stl的演讲比赛流程管理系统/演讲比赛流程管理系统.cpp
+++ 基于stl的演讲比赛流程管理系统/演讲比赛流程管理系统.cpp
@@ -1,37 +1,18 @@
-//����������
-//��������1.�ṩ�˵��������û����� 2.���ݽ��������̿��� 3.���ļ��Ķ�д����
-//��Ҫ��������ϵͳ����
+
 #include<bits/stdc++.h>
 #include"speechManager.h"
 using namespace std;
 int main()
 {
-	//�������������
 	SpeechManager sm;
 
-	//���ÿ�����ݶ���һ�������
-
-	//�������������
 	srand((unsigned int)time(NULL));
-	//����12��
-	//
-	// for (auto s : sm.m_speaker)
-	//{
-		//cout << "ѡ�ֱ��"<<s.first << "    ѡ������"<<s.second.m_name << "    ѡ�ֳɼ�"<<s.second.m_score[0]<<endl;
-	//}
-	//for (map<int,SpeaKer>::iterator it=sm.m_speaker.begin();it!=sm.m_speaker.end();it++)
-	//{
-		//cout << "ѡ�ֱ��:" << it->first << "ѡ������;" << it->second.m_name <<"����:"<<it->second.m_score[0]<< endl;
-	//}
-	//sm.show_Meun();
-	//�������������û�������
 	int choice = 0;
 	
-	//��֤�ظ�����ϵͳ��ֻ����0��ʱ���˳�ϵͳ
 	while (true)
 	{
-		sm.show_Meun();//������������ܱ�֤������˵��ٴγ���
-		cout << "���������ѡ��" << endl;
+		sm.show_Meun();
+		cout << "choice" << endl;
 		cin >> choice;
 		switch (choice)
 		{
